0ce2c206ba02438e1a5153c42889c88c1a358472
vermajay <vermajay6604@gmail.com>
2024-06-22 16:57:32 +0530

    first commit

---
+++ src/firebase-config.ts
@@ -0,0 +1,11 @@
+// TODO: Replace with your config
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxWpDUuTCXEkMuHF0FibU8Wi2ftEH0l48",
+    authDomain: "test1-q7tye.firebaseapp.com",
+    projectId: "test1-q7tye",
+    storageBucket: "test1-q7tye.appspot.com",
+    messagingSenderId: "469922357595",
+    appId: "1:469922357595:web:496a81ff24fc7594973a7e",
+};
+
+//   measurementId: "G-M18DTL63RT"
